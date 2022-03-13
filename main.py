ef91931633113005d20986cec656cea25b90c018
Matthew Bunce <mbunce@uoguelph.ca>
2022-03-13 16:51:26 -0400

    added imports

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+import requests
 from bs4 import BeautifulSoup
 
-linkOne = "https://www.kijiji.ca/b-dogs-puppies/ontario/hypoallergenic/k0c126l9004?rb=true"
-linkTwo = "https://www.kijiji.ca/b-dogs-puppies/ontario/hypoallergenic/page-2/k0c126l9004?rb=true"
-linkThree = "https://www.kijiji.ca/b-dogs-puppies/ontario/hypoallergenic/page-3/k0c126l9004?rb=true"
+URL = "https://www.kijiji.ca/b-dogs-puppies/ontario/hypoallergenic/page-1/k0c126l9004?rb=true""
 
-requestPage = urlopen(linkOne) 
+requestPage = requests.get(URL) 
 pageHTML = requestPage.read()
 requestPage.close()
 
