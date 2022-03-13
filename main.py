07b5c10eae7ca0cad0eb7be6d43446a347b8a534
Matthew Bunce <mbunce@uoguelph.ca>
2022-03-13 16:11:37 -0400

    changes

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from bs4 import BeautifulSoup
 
-with open('home.html', 'r') as htmlFile:
-    content = htmlFile.read()
-    
+linkOne = "https://www.kijiji.ca/b-dogs-puppies/ontario/hypoallergenic/k0c126l9004?rb=true"
+linkTwo = "https://www.kijiji.ca/b-dogs-puppies/ontario/hypoallergenic/page-2/k0c126l9004?rb=true"
+linkThree = "https://www.kijiji.ca/b-dogs-puppies/ontario/hypoallergenic/page-3/k0c126l9004?rb=true"
+
+requestPage = urlopen(linkOne) 
+pageHTML = requestPage.read()
+requestPage.close()
+
+html_soup = BeautifulSoup(pageHTML, 'html.parser')
