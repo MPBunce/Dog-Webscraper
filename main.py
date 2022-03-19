f08444a1ccf4819eb8969ed22c2d7763761d4511
Matthew Bunce <mbunce@uoguelph.ca>
2022-03-19 17:21:22 -0400

    dogDict.csv

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import requests
 import re
+import requests
+import pandas as pd
 from bs4 import BeautifulSoup
-from html.parser import HTMLParser
 
 URL = "https://www.kijiji.ca/b-dogs-puppies/ontario/hypoallergenic/page-1/k0c126l9004?rb=true"
 requestPage = requests.get(URL) 
@@ -16,6 +16,7 @@ print("The total number of pages is: "+ str(totalPages))
 partOneUrl = "https://www.kijiji.ca/b-dogs-puppies/ontario/hypoallergenic/page-"
 partTwoUrl = "/k0c126l9004?rb=true"
 
+data = []
 
 for page in range(1, totalPages + 1):
 
@@ -26,11 +27,16 @@ for page in range(1, totalPages + 1):
 
     requestPage = requests.get(url) 
     doc = BeautifulSoup(requestPage.text, "html.parser")
-    dogPrice = doc.find_all(class_="price")
-    dogTitle = doc.find_all(class_="title")
 
-    items_found[]
+    # info container
+    info_container = doc.find_all(class_="info-container")
+    for ad in info_container:
+        dogPrice = ad.find(class_="price").get_text().strip()
+        dogTitle = ad.find(class_="title").get_text().strip()
+        dogLink = 'kijiji.ca'+ ad.find('a').get('href')
+        data.append([dogTitle,dogPrice,dogLink])
+        
+df=pd.DataFrame(data, columns=['Title', 'Price', 'Link'])
 
-
-
-  
+df.to_csv('dogDict.csv')
+print('csv Exported')
