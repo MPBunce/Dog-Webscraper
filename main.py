b986f3e63746dac6a7a62411d613233a53cba230
Matthew Bunce <mbunce@uoguelph.ca>
2022-03-19 13:10:51 -0400

    chnages

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://www.kijiji.ca/b-dogs-puppies/ontario/hypoallergenic/page-1/k0c126l9004?rb=true""
-
+URL = "https://www.kijiji.ca/b-dogs-puppies/ontario/hypoallergenic/page-1/k0c126l9004?rb=true"
 requestPage = requests.get(URL) 
-pageHTML = requestPage.read()
-requestPage.close()
+doc = BeautifulSoup(requestPage.text, "html.parser")
 
-html_soup = BeautifulSoup(pageHTML, 'html.parser')
+numPages = doc.find(class_="pagination")
+splitOne = str(numPages).split(">")
+splitTwo = str(splitOne).split("</a")[-4]
+totalPages = int(str(splitTwo).split("'")[-1])
+print(totalPages)
+
+for page in range(1, totalPages + 1):
+    URL = "https://www.kijiji.ca/b-dogs-puppies/ontario/hypoallergenic/page-{page}/k0c126l9004?rb=true"
+    requestPage = requests.get(URL) 
+    doc = BeautifulSoup(requestPage.text, "html.parser")
+
+    items = doc.find_all(text=re.compile())
