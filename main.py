57aa53bcea070f6e0c7c4d9f53d01b3f85ccaed7
Matthew Bunce <mbunce@uoguelph.ca>
2022-03-19 15:47:41 -0400

    update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+from html.parser import HTMLParser
 
 URL = "https://www.kijiji.ca/b-dogs-puppies/ontario/hypoallergenic/page-1/k0c126l9004?rb=true"
 requestPage = requests.get(URL) 
@@ -10,7 +11,7 @@ numPages = doc.find(class_="pagination")
 splitOne = str(numPages).split(">")
 splitTwo = str(splitOne).split("</a")[-4]
 totalPages = int(str(splitTwo).split("'")[-1])
-print(totalPages)
+print("The total number of pages is: "+ str(totalPages))
 
 partOneUrl = "https://www.kijiji.ca/b-dogs-puppies/ontario/hypoallergenic/page-"
 partTwoUrl = "/k0c126l9004?rb=true"
@@ -19,11 +20,17 @@ partTwoUrl = "/k0c126l9004?rb=true"
 for page in range(1, totalPages + 1):
 
     num = str(page)
-    print(num)
+    print("The number of iterations is " + str(num))
+    
     url = partOneUrl+num+partTwoUrl
-    print(url)
+
     requestPage = requests.get(url) 
     doc = BeautifulSoup(requestPage.text, "html.parser")
-    testPageNumber = doc.find(class_="pagination")
-    print(testPageNumber)
+    dogPrice = doc.find_all(class_="price")
+    dogTitle = doc.find_all(class_="title")
+
+    items_found[]
+
+
+
   
